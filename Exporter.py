33cfa7c3c318f5f05e143be770c2f19700093396
Will Jones <willjones1989@gmail.com>
2022-04-29 10:24:59 -0400

    Use NULL instead of exptry string

--- Exporter.py
+++ Exporter.py
@@ -7,43 +7,62 @@ class Exporter:
         print("Init:     Exporter")
 
     def exportXLSX(self, df, fileName):
+        # create xlsx file from df
         df.to_excel(fileName, index=False)
-
-    def getValueString(self, value, isLast):
-        return "'" + str(value) + "'" + ("" if isLast else ", ")
     
     def exportSQL(self, df, fileName):
+        # create sql string for creating table
         sql = '''
-CREATE TABLE fall_2022 (
-    block varchar(16),
-    dep varchar(16) NOT NULL,
-    course varchar(16) NOT NULL,
-    section varchar(16) NOT NULL,
-    title varchar(256) NOT NULL,
-    instructor varchar(64) NOT NULL,
-    day varchar(16) NOT NULL,
-    start time,
-    end time,
-    size int NOT NULL DEFAULT 0,
-    bldg varchar(16) NOT NULL,
-    loc varchar(16) NOT NULL,
-    rm varchar(16) NOT NULL,
-    comments varchar(1024)
+CREATE TABLE `fall_2022` (
+    `block` varchar(16),
+    `dep` varchar(16) NOT NULL,
+    `course` varchar(16) NOT NULL,
+    `section` varchar(16) NOT NULL,
+    `title` varchar(256) NOT NULL,
+    `instructor` varchar(64) NOT NULL,
+    `day` varchar(16) NOT NULL,
+    `start` time,
+    `end` time,
+    `size` int NOT NULL DEFAULT 0,
+    `bldg` varchar(16) NOT NULL,
+    `loc` varchar(16) NOT NULL,
+    `rm` varchar(16) NOT NULL,
+    `comments` varchar(1024)
 );
-
-INSERT INTO fall_2022 VALUES
 '''
-        rows = df.values.tolist()
-        for row in range(len(rows)):
-            sql += "("
-            for col in range(len(rows[row])):
-                sql += self.getValueString(rows[row][col], col == len(rows[row]) - 1)
 
+        # add sql string for inserting values
+        sql += "INSERT INTO `fall_2022` VALUES\n"
+
+        # get 2d array of values for df
+        rows = df.values.tolist()
+
+        # iterate over each row of the rows
+        for row in range(len(rows)):
+            # add sql string for row values
+            sql += "("
+
+            # iterate over each col of the row
+            for col in range(len(rows[row])):
+                # get value of col as a string
+                strValue = str(rows[row][col])
+
+                # if strValue is an empty string ("") return NULL,
+                # otherwise return strValue wrapped in single quotes
+                sql += "NULL" if strValue == "" else "'" + strValue + "'"
+
+                # add comma if not the last col
+                if col != len(rows[row]) - 1:
+                    sql += ", "
+            
+            # end sql string for row values.
+            # add comma if not the last row
             if row == len(rows) - 1:
                 sql += ");\n"
             else:
                 sql += "),\n"
-
+        
+        # create sql file from sql string
         db = open(fileName, "w")
         db.write(sql)
         db.close
